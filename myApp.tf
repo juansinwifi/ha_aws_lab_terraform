f24380186a4b3d2cb9c04d277fdd72ad3289ab37
Juan Sebastian Otero <juan.otero@globant.com>
2020-05-04 15:56:09 -0500

    Change source moduel s3 backend

--- myApp.tf
+++ myApp.tf
@@ -3,7 +3,8 @@ provider "aws" {
 }
 
 module "s3_bucket_backend" {
-  source        = "./modules/s3_backend"
+  //source        = "./modules/s3_backend"
+  source        = "github.com/juansinwifi/terraform_modules_aws/s3_backend"
   bucket_name   = var.bucket_name_backend
   bucket_acl    = var.bucket_acl_backend
   tags          = var.tags_backend
