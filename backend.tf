3ff5f6b3672696cb4efa2350eed7ddd83cdfbca3
Juan Sebastian Otero <juan.otero@globant.com>
2020-05-04 20:50:53 -0500

    fix backend file

--- backend.tf
+++ backend.tf
@@ -1,7 +1,9 @@
 terraform {
-    backend "s3" {
-        bucket = var.bucket_name_backend
-        key = "dev"
-        region = var.region
-        }
+  backend "s3" {
+    bucket          = "tf-lab-juansinwifiapp-20-backend"
+    key             = "dev"
+    region          = "us-east-1"
+    encrypt         = true
+    kms_key_id      = "arn:aws:kms:us-east-1:620976139467:key/09439b35-8ff3-4ecc-9898-27227a07d35d"
+  }
 }
